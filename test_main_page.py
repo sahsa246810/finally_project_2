983bb00a9d10559455eb64213bbcb3b2ab99da69
sahsa246810 <sahsa.antipov.99@mail.ru>
2023-10-01 18:11:43 +0300

    add file

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    url = "http://selenium1py.pythonanywhere.com/"
+    browser.get(url)
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
